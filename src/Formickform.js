aeb5db456353728db20968b35b715d2beeeefc98
RAHUL SHAW <50042872+rahulbuccha@users.noreply.github.com>
2022-02-23 12:09:20 +0530

    initial commit

--- src/Formickform.js
+++ src/Formickform.js
@@ -1,82 +1,81 @@
 import React from 'react';
-import {Formik,Form,Field} from 'formik';
+import { Formik, Form, Field } from 'formik';
 import * as Yup from 'yup'
+
 
 function FormikYup() {
     function validateEmail(value) {
         let error;
         if (!value) {
-          error = 'Required';
+            error = 'Required';
         } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)) {
-          error = 'Invalid email address';
+            error = 'Invalid email address';
         }
         return error;
-      }
-     
+    }
+
 
     const SignUpSchema = Yup.object().shape(
         {
-            firstname: Yup.string()
-            .min(5,'Should be 5 character long')
-            .max(15,'should not exceed 15 characters')
-            .required('Required'),
+            name: Yup.string()
+                .max(10, 'should not exceed 10 characters')
+                .required('Required'),
+            number: Yup.string()
+                .min(10, 'perfect')
+                .max(10, 'should not exceed 10 characters')
+                .required('Required'),
 
-            lastname: Yup.string()
-            .min(4,'Should be 4 character long')
-            .max(15,'should not exceed 15 characters')
-            .required('Required'),
-
-            email:Yup.string()
-            .email('invalid email address')
-            .required('Required'),
+            email: Yup.string()
+                .email('invalid email address')
+                .required('Required'),
 
             password: Yup.string()
-            .required('Please Enter your password')
-            .matches(
-            /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})/,
-            "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character")
+                .required('Please Enter your password')
+                .matches(
+                    /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})/,
+                    "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character")
         }
     );
     return (
         <div className='center'>
             <p><ins>Registration Form</ins></p>
             <Formik
-            initialValues = {
-                {
-                firstname:'',
-                lastname:'',
-                email:'',
-                password:''
+                initialValues={
+                    {
+                        name: '',
+                        number: '',
+                        email: '',
+                        password: ''
+                    }
                 }
-            }
-            validationSchema = {SignUpSchema}
-            onSubmit = {values => {
-                alert(values);
-            }}
+                validationSchema={SignUpSchema}
+                onSubmit={values => {
+                    alert(values);
+                }}
             >
-            { ({errors,touched}) => (
-                <Form>
-                 <Field name="firstname" placeholder="Enter Your First Name"/>
-                 {
-                     errors.firstname && touched.firstname ? (<div>{errors.firstname}</div>) : null
-                 }<br></br><br></br>
-                 <Field name="lastname"placeholder="Enter Your Last Name" />
-                 {
-                     errors.lastname && touched.lastname ? (<div>{errors.lastname}</div>) : null
-                 }<br></br><br></br>
-                 <Field name="email"placeholder='Email Address' validate={validateEmail} />
-                 {
-                     errors.email && touched.email ? (<div>{errors.email}</div>) : null
-                 }<br></br><br></br>
-                 <Field name="password"placeholder='Enter Any Password' />
-                 {
-                     errors.password && touched.password ?(<div>{errors.password}</div>) :null
-                 }<br></br><br></br>
-                 
-                 <button type="submit" className="btn btn-success">Submit</button>
-                 </Form>
-            )}
+                {({ errors, touched }) => (
+                    <Form>
+                        <Field name="name" placeholder="Enter Your Name" />
+                        {
+                            errors.name && touched.name ? (<div>{errors.name}</div>) : null
+                        }<br></br><br></br>
+                        <Field name="number" placeholder="Enter Your Number" />
+                        {
+                            errors.number && touched.number ? (<div>{errors.number}</div>) : null
+                        }<br></br><br></br>
 
+                        <Field name="email" placeholder='Email Address' validate={validateEmail} />
+                        {
+                            errors.email && touched.email ? (<div>{errors.email}</div>) : null
+                        }<br></br><br></br>
+                        <Field name="password" placeholder='Enter Any Password' minlength='10' />
+                        {
+                            errors.password && touched.password ? (<div>{errors.password}</div>) : null
+                        }<br></br><br></br>
+
+                        <button type="submit" className="btn btn-success">Register</button>
+                    </Form>
+                )}
             </Formik>
         </div>
     )
